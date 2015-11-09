f6026b22983a0802b215a92ce01ee658c19da392
agermenos <agermenos@gmail.com>
2015-11-09 15:57:57 -0800

    Minor fixes Added recycleview library

--- app/src/main/java/com/udacity/immuno/activities/MainActivity.java
+++ app/src/main/java/com/udacity/immuno/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package udacity.com.immuno;
+package com.udacity.immuno.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import udacity.com.immuno.R;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/udacity/immuno/activities/MainActivityFragment.java
+++ app/src/main/java/com/udacity/immuno/activities/MainActivityFragment.java
@@ -1,10 +1,12 @@
-package udacity.com.immuno;
+package com.udacity.immuno.activities;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import udacity.com.immuno.R;
 
 /**
  * A placeholder fragment containing a simple view.
