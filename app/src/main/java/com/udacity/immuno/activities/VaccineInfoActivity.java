3b6efc0578635457e5baf9798c5c964421a8bb56
sengopal <senthil777@gmail.com>
2015-11-10 14:43:39 -0800

    adding vaccine info

--- app/src/main/java/com/udacity/immuno/activities/VaccineInfoActivity.java
+++ app/src/main/java/com/udacity/immuno/activities/VaccineInfoActivity.java
@@ -1,14 +1,18 @@
 package com.udacity.immuno.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ProgressBar;
 
 import com.udacity.immuno.R;
 import com.udacity.immuno.database.DBHelper;
 import com.udacity.immuno.database.VaccineData;
 
 public class VaccineInfoActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,11 @@ public class VaccineInfoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        VaccineData vaccineId = getIntent().getParcelableExtra("vaccineInfo");
+        VaccineData vaccineData = getIntent().getParcelableExtra("vaccineInfo");
         long userId = getIntent().getLongExtra("userId", DBHelper.getPrimaryUserId());
-        
+
+        //progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        //progressBar.setVisibility(View.VISIBLE);
 
         /* No Fab button in this Activity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -32,7 +38,65 @@ public class VaccineInfoActivity extends AppCompatActivity {
         });
         */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(vaccineData.getCasualName());
 
+    }
+    /*
+    public class AsyncHttpTask extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected void onPreExecute() {
+            progressBar.setIndeterminate(true);
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            Integer result = 0;
+            HttpURLConnection urlConnection;
+            try {
+                if (params[0]!=null && !"empty".equals(params[0])) {
+                    URL url = new URL(params[0]);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    int statusCode = urlConnection.getResponseCode();
+
+                    // 200 represents HTTP OK
+                    if (statusCode == 200) {
+                        BufferedReader r = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = r.readLine()) != null) {
+                            response.append(line);
+                        }
+                        parseResult(response.toString());
+                        result = 1; // Successful
+                    } else {
+                        result = 0; //"Failed to fetch data!";
+                    }
+                }
+                else {
+                    parseResult(new Dummy().getData());
+                    result=1;
+                }
+            } catch (Exception e) {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+            return result; //"Failed to fetch data!";
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            // Download complete. Let us update UI
+            progressBar.setVisibility(View.GONE);
+
+            if (result == 1) {
+                adapter = new RecycleViewAdapter(getContext(), vaccineDataList);
+                mRecyclerView.setAdapter(adapter);
+            } else {
+                Toast.makeText(getActivity(), "Failed to fetch data!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    */
 
 }
