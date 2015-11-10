50f5ba56fdbf54edfcdd4940ba413294de2f7936
agermenos <agermenos@gmail.com>
2015-11-10 11:36:16 -0800

    Removing floating from Search

--- app/src/main/java/com/udacity/immuno/activities/SearchActivity.java
+++ app/src/main/java/com/udacity/immuno/activities/SearchActivity.java
@@ -22,15 +22,6 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
