ae6af169ba6d0cd529c590d51b745f98a6913334
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2015-11-10 09:14:15 -0800

    add navigation drawer

--- app/src/main/java/com/udacity/immuno/activities/MainActivity.java
+++ app/src/main/java/com/udacity/immuno/activities/MainActivity.java
@@ -2,7 +2,9 @@ package com.udacity.immuno.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -11,25 +13,77 @@ import android.view.View;
 
 import com.udacity.immuno.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Bind(R.id.navigation_view) NavigationView mNavigationView;
+    @Bind(R.id.drawer_layout) DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+        setUpToolbar();
+        setUpNavigation();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //TODO: start search, how ya wanna do this Alejandro?
             }
         });
     }
+
+    private void setUpToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //TODO: set title based on user’s first name
+        getSupportActionBar().setTitle("Alejandro’s Record");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+    }
+
+    private void setUpNavigation(){
+
+        //TODO: set up account information from db
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(final MenuItem menuItem) {
+
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_item_parent1:
+                        //TODO: create new fragment
+                        break;
+
+                    case R.id.nav_item_parent2:
+                        //TODO: create new fragment
+                        break;
+                    case R.id.nav_item_child1:
+                        //TODO: create new fragment
+                        break;
+
+                    case R.id.nav_item_child2:
+                        //TODO: create new fragment
+                        break;
+                } //switch
+
+                //TODO: add fragment to FrameLayout
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                return true;
+            } //onNavigationItemSelected
+        });
+    } //setUpNavigation
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,9 +99,15 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(id){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_share:
+                //TODO: Gurmeet: add export as PDF?
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
