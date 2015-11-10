19529235499ea8f7071ced4c5d7cd23eca6374cb
agermenos <agermenos@gmail.com>
2015-11-10 11:32:15 -0800

    Fix after merge

--- app/src/main/java/com/udacity/immuno/activities/SearchFragment.java
+++ app/src/main/java/com/udacity/immuno/activities/SearchFragment.java
@@ -22,6 +22,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
