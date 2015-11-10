54fe2e1e6b06a9b2f1327a63921535966373ce5a
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2015-11-10 14:36:51 -0800

    add adapter for main screen, updated assets

--- app/src/main/java/com/udacity/immuno/activities/MainActivityFragment.java
+++ app/src/main/java/com/udacity/immuno/activities/MainActivityFragment.java
@@ -1,17 +1,33 @@
 package com.udacity.immuno.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.udacity.immuno.R;
+import com.udacity.immuno.adapters.MainActivityAdapter;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    public static final String TAG = MainActivityFragment.class.getSimpleName();
+
+    private Context mContext;
+
+    private LinearLayoutManager mLayoutManager;
+    private MainActivityAdapter mAdapter;
+
+    @Bind(R.id.recycler_view) RecyclerView mRecyclerView;
 
     public MainActivityFragment() {
     }
@@ -19,7 +35,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        mContext = getActivity();
+        ButterKnife.bind(this, rootView);
+
+        setUpRecyclerView();
+
+        return rootView;
+
+    }
+
+    public void setUpRecyclerView(){
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mLayoutManager = new LinearLayoutManager(mContext);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //TODO: create adapter
+        //mAdapter  = new MainActivityAdapter(getActivity(), );
+        //mRecyclerView.setAdapter(MainActivityAdapter);
+
+        mRecyclerView.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS); //for accessibility
     }
 }
---
+++ app/src/main/java/com/udacity/immuno/adapters/MainActivityAdapter.java
@@ -0,0 +1,148 @@
+package com.udacity.immuno.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.udacity.immuno.R;
+import com.udacity.immuno.database.VaccineData;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by alex on 15-11-10.
+ */
+public class MainActivityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<VaccineData> vaccineDataList;
+    private List<Integer> views;
+    private Context mContext;
+
+    private static final int TYPE_HEADER_NEEDS_ATTENTION = 0;
+    private static final int TYPE_HEADER_UPCOMING = 1;
+    private static final int TYPE_HEADER_PAST_VACCINES = 2;
+    private static final int TYPE_BUTTON_MORE = 3;
+    private static final int TYPE_VACCINE = 4;
+
+
+    public MainActivityAdapter(Context context, List<VaccineData> vaccineDataList) {
+        this.vaccineDataList = vaccineDataList;
+        this.mContext = context;
+
+        setViews();
+    }
+
+    public void setViews(){
+
+        //TODO: check if there are need attention items
+            //if yes: views.add(TYPE_HEADER_NEEDS_ATTENTION);
+        //TODO: for every vaccine that needs attention:
+            //views.add(TYPE_VACCINE);
+        //TODO: rinse repeat
+
+        views.add(TYPE_BUTTON_MORE); //add at the end of the list
+
+    } //setViews
+
+    public static class HeaderViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.header_title) TextView headerTitle;
+
+        public HeaderViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+    public static class VaccineViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.vaccine_icon) ImageView vaccineIcon;
+        @Bind(R.id.vaccine_name) TextView vaccineName;
+        @Bind(R.id.vaccine_formal_name) TextView vaccineFormalName;
+        @Bind(R.id.vaccine_action)ToggleButton vaccineAction;
+
+        public VaccineViewHolder(View v) {
+            super(v);
+            ButterKnife.bind(this, v);
+        }
+    }
+
+    public static class MoreButtonViewHolder extends RecyclerView.ViewHolder {
+        public MoreButtonViewHolder(View v) {
+            super(v);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater mInflater = LayoutInflater.from(parent.getContext());
+
+        switch (viewType) {
+
+            case TYPE_HEADER_NEEDS_ATTENTION:
+            case TYPE_HEADER_UPCOMING:
+            case TYPE_HEADER_PAST_VACCINES:
+                View headerView = mInflater.inflate(R.layout.header_recyclerview, parent, false);
+                return new HeaderViewHolder(headerView);
+
+            case TYPE_BUTTON_MORE:
+                View buttonMoreView = mInflater.inflate(R.layout.row_more_button, parent, false);
+                return new MoreButtonViewHolder(buttonMoreView);
+
+            case TYPE_VACCINE:
+                View vaccineView = mInflater.inflate(R.layout.row_vacine_small, parent, false);
+                return new VaccineViewHolder(vaccineView);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        switch (holder.getItemViewType()) {
+
+            case TYPE_HEADER_NEEDS_ATTENTION:
+                final HeaderViewHolder headerViewHolder = (HeaderViewHolder) holder;
+                headerViewHolder.headerTitle.setText("");
+
+
+                break;
+
+            case TYPE_HEADER_UPCOMING:
+
+                break;
+
+            case TYPE_HEADER_PAST_VACCINES:
+
+                break;
+
+            case TYPE_VACCINE:
+                final VaccineViewHolder vaccineView = (VaccineViewHolder) holder;
+
+                //TODO: set data for this vaccine
+                //Vaccine vaccine = vaccineDataList.get(position - 1); //account for header view
+                vaccineView.vaccineName.setText("");
+                vaccineView.vaccineFormalName.setText("");
+
+                //TODO: set icon and set action
+                break;
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != vaccineDataList ? vaccineDataList.size() : 0);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return views.get(position);
+    }
+}
