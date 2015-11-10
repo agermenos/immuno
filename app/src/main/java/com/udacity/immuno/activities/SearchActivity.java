f64777a26b057ffa91881b929d426ffa2862a7ed
agermenos <agermenos@gmail.com>
2015-11-10 10:57:18 -0800

    Improving Search List Items

--- app/src/main/java/com/udacity/immuno/activities/SearchActivity.java
+++ app/src/main/java/com/udacity/immuno/activities/SearchActivity.java
@@ -1,10 +1,15 @@
 package com.udacity.immuno.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.udacity.immuno.R;
@@ -26,6 +31,22 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
     }
 
 }
--- app/src/main/java/com/udacity/immuno/adapters/CustomViewHolder.java
+++ app/src/main/java/com/udacity/immuno/adapters/CustomViewHolder.java
@@ -2,6 +2,8 @@ package com.udacity.immuno.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.udacity.immuno.R;
@@ -12,9 +14,15 @@ import com.udacity.immuno.R;
 public class CustomViewHolder extends RecyclerView.ViewHolder {
 
     protected TextView tvName;
+    protected ImageView ibVaccineIcon;
+    protected TextView tvFormalName;
+    protected ImageView ibVaccineAction;
 
     public CustomViewHolder(View view) {
         super(view);
-        this.tvName = (TextView) view.findViewById(R.id.name);
+        this.tvName = (TextView) view.findViewById(R.id.vaccine_name);
+        this.ibVaccineIcon = (ImageView) view.findViewById(R.id.vaccine_icon);
+        this.tvFormalName = (TextView) view.findViewById(R.id.vaccine_formal_name);
+        this.ibVaccineAction = (ImageView) view.findViewById(R.id.vaccine_action);
     }
 }
